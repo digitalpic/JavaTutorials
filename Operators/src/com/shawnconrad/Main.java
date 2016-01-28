8912e9119ea356e726369c362246883c2ed28811
Shawn Conrad <digitalpic@me.com>
2016-01-28 16:53:14 -0600

    Added more operationals and exercise

--- Operators/src/com/shawnconrad/Main.java
+++ Operators/src/com/shawnconrad/Main.java
@@ -51,16 +51,70 @@ public class Main {
         // Assignment operator set value to false. Conditional code.
         boolean isAlien = false;
         // Testing to see if value is false.
-        if (isAlien == true)
+        if (    isAlien == false)
             System.out.println("It is not an alien.");
-        // Have to be true
+        // Has to be true
         int topScore = 80;
         if (topScore < 100)
             System.out.println("You've got the top score");
-        //
-        int secondTopScore = 60;
-        if (topScore > secondTopScore && topScore < 100)
+        // && must be true compare AND
+        int secondTopScore = 95;
+        if ((topScore > secondTopScore) && (topScore < 100))
             System.out.println("Greater than second top score, and less than 100.");
+        // OR ||
+        if ((topScore >90) || (secondTopScore <=90))
+        System.out.println("One of these test is true");
+
+        int newValue = 50;
+        // One equal '=' means setting value.
+        if (newValue == 50)
+            System.out.println("This is true!");
+
+        boolean isCar = false;
+        if(isCar == true)
+            System.out.println("This is not suppose to happen");
+
+        // Turnery operator (shortcut)
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        System.out.println("wasCar is true");
+
+        // 1. Create a double variable with a value of 20
+        // 2. Create a second variable  of type double with a value of 80
+        // 3. Add both numbers up and multiply by 25
+        // 4. Use the remainder operator to figure out the remainder from the sum of #3 divided by 40
+        // 5. Write an "if" statement that displays the message, "Total was over the limit".
+        //    if the remainder total (#4) is equal to 20 or less.
+
+        // PROPER ANSWER
+        double myFirstValue = 20d;
+        double mySecondValue = 80;
+        // Precedence. Calculate in brackets first.
+        double myTotal = (myFirstValue + mySecondValue) * 25;
+        System.out.println("myTotal = " + myTotal);
+        double theRemainder = myTotal % 40;
+        System.out.println("Remainder is "  + theRemainder);
+        if(theRemainder <=20)
+            System.out.println("Total was over the limit.");
+
+
+        // My initial answer
+        int newResult = 20 + 80;
+        System.out.println("20 + 80 = " + newResult);
+
+        int prevResult = newResult;
+
+        newResult = newResult * 25;
+        System.out.println(prevResult + " * 25 = " + newResult);
+
+        prevResult = newResult;
+
+        newResult = newResult % 40;
+        System.out.println(prevResult + " Remainder = " + newResult);
+
+        if (newResult <= 20)
+            System.out.println("Total was over the limit.");
+
 
     }
 }
